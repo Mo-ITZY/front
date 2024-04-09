8d879f3c11d20c39b6c8aef24562c6126983e1ba
hyeon0820 <13whdduq13@naver.com>
2024-04-09 15:56:14 +0900

    Create Intro

---
+++ src/components/Intro/Introroot.jsx
@@ -0,0 +1,19 @@
+﻿import styles from './Introroot.module.css';
+import Main_Logo from "./image/Mo_ITZY.svg?react";
+import { NavLink } from "react-router-dom";
+
+function Introroot() {
+  return (
+    <div>
+        <div><Main_Logo /></div>
+        <NavLink to='./login'>
+            <div className={styles.Login_Box} >로그인</div>
+        </NavLink>
+        <NavLink to='/main'>
+            <div className={styles.Look_Around_Box}>둘러보기</div>
+        </NavLink>
+    </div>
+  )
+}
+
+export default Introroot
---
+++ src/components/login/Loginroot.jsx
@@ -0,0 +1,11 @@
+﻿//import styles from './Loginroot.module.css';
+
+function Loginroot() {
+  return (
+    <div>
+        로그인
+    </div>
+  )
+}
+
+export default Loginroot
