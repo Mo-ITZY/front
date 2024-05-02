a3be817995fc53bd1a9cc3f71ae840a83a4240a4
tlswl7479 <tlswl7479@gmail.com>
2024-05-02 18:01:31 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import './App.css'
 import Intro from './components/Intro/Introroot'
 import Main from './components/main/Mainroot'
 import Login from './components/login/Loginroot'
+import Search from './components/search/Searchroot'
 import { useMediaQuery } from "react-responsive"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-
-
 
 
   function App() {
@@ -23,6 +22,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
             <Route path="/" element={isPC ? <Intro /> : isMobile && <Intro />} />
             <Route path="/main" element={isPC ? <Main /> : isMobile && <Main />} />
             <Route path="/login" element={isPC ? <Login /> : isMobile && <Login />} />
+            <Route path="/search" element={isPC ? <Search /> : isMobile && <Search />} />
           </Routes>
         </Router>
       </>
