810b13b63ff953f4c12bafed06977543890a9b90
hyeon0820 <13whdduq13@naver.com>
2024-05-07 11:52:11 +0900

    festaival view no detail

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,34 @@ import './App.css'
 import Intro from './components/Intro/Introroot'
 import Main from './components/main/Mainroot'
 import Login from './components/login/Loginroot'
+import All from './components/all/allroot'
+//import Allview from './components/all/allrootview/allrootview'
 import { useMediaQuery } from "react-responsive"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 
 
 
-  function App() {
-    const isMobile = useMediaQuery({
+function App() {
+  const isMobile = useMediaQuery({
       query : "(max-width:768px)"
     });
 
-    const isPC = useMediaQuery({
+  const isPC = useMediaQuery({
       query : "(min-width:769px)"
     });
-    return (
-      <>
-        <Router>
-          <Routes>
-            <Route path="/" element={isPC ? <Intro /> : isMobile && <Intro />} />
-            <Route path="/main" element={isPC ? <Main /> : isMobile && <Main />} />
-            <Route path="/login" element={isPC ? <Login /> : isMobile && <Login />} />
-          </Routes>
-        </Router>
-      </>
+  return (
+    <>
+       <Router>
+        <Routes>
+          <Route path="/" element={isPC ? <Intro /> : isMobile && <Intro />} />
+          <Route path="/main" element={isPC ? <Main /> : isMobile && <Main />} />
+          <Route path="/login" element={isPC ? <Login /> : isMobile && <Login />} />
+          <Route path="/all" element={isPC ? <All /> : isMobile && <All />} />
+          
+        </Routes>
+      </Router>
+    </>
     )
   }
 
--- src/components/main/Mainroot.jsx
+++ src/components/main/Mainroot.jsx
@@ -1,9 +1,12 @@
-﻿//import styles from './Mainroot.module.css';
+﻿import styles from './Mainroot.module.css';
+import { NavLink } from "react-router-dom";
 
 function Mainroot() {
   return (
     <div>
-        메인
+          <NavLink to='/all'>
+            <div className={styles.Look_Around_Box}>각 지역축제 더보기</div>
+        </NavLink>
     </div>
   )
 }
