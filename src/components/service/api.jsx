be52c100b83397933ed1f50d3be9c9fd9d81060a
hyeon0820 <13whdduq13@naver.com>
2024-05-07 12:11:45 +0900

    aa

---
+++ src/components/service/api.jsx
@@ -0,0 +1,21 @@
+﻿import axios from "axios";
+
+const fetchData = async() => {
+    try{
+        const URL = "http://apis.data.go.kr/6260000/FestivalService/getFestivalKr";
+        const response = await axios.get(URL, {
+            params: {
+                serviceKey : decodeURIComponent(import.meta.env.VITE_API_KEY),
+                numOfRows: 10,
+                pageNo: 1,
+                resultType : "JSON"
+            }
+        });
+        return response.data;
+    }
+    catch(error){
+        throw error.response.data;
+    }
+};
+
+export default fetchData;
