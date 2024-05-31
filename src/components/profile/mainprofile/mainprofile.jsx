c73914ed056409ca726f4b5a5ff7f2d8afb8a313
hyeon0820 <13whdduq13@naver.com>
2024-05-31 15:11:37 +0900

    profile

---
+++ src/components/profile/mainprofile/mainprofile.jsx
@@ -0,0 +1,21 @@
+﻿import styles from './mainprofile.module.css';
+
+function MainProfile() {
+  return (
+    <div>
+      <div>
+          <div className={styles.main_box}>
+              <div className={styles.profile}></div>
+              <div className={styles.name}>OOO님</div>
+              <div className={styles.review}>작성하신 리뷰 : 4개</div>
+              <div className={styles.location}>
+                  <div className={styles.change}>회원정보 수정</div>
+                  <div className={styles.logout}>로그아웃</div>
+              </div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default MainProfile;
