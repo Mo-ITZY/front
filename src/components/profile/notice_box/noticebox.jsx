f94d306736859b526e3408af717f50634ec126a5
hyeon0820 <13whdduq13@naver.com>
2024-06-01 17:25:14 +0900

    next button and before button

--- src/components/profile/notice_box/noticebox.jsx
+++ src/components/profile/notice_box/noticebox.jsx
@@ -9,11 +9,11 @@ function noticebox() {
         <div>
             <div className={styles.notice_box}>
                 <div className={styles.detail_box}>공지사항
-                <MdArrowForwardIos style={{marginLeft: '230px'}} /></div>
+                <MdArrowForwardIos style={{marginLeft: '228px'}} /></div>
                 <div className={styles.detail_box}>1:1 문의
                 <MdArrowForwardIos style={{marginLeft: '230px'}} /></div>
                 <div className={styles.detail_box}>자주 묻는 질문
-                <MdArrowForwardIos style={{marginLeft: '200px'}} /></div>
+                <MdArrowForwardIos style={{marginLeft: '190px'}} /></div>
             </div>
         </div>
     </div>
