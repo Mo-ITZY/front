7de17310b3c87b143ba1f5e37d9857dcb7910cdd
hyeon0820 <13whdduq13@naver.com>
2024-06-10 00:18:23 +0900

    main_photo

--- src/components/main/Mainroot.jsx
+++ src/components/main/Mainroot.jsx
@@ -2,19 +2,24 @@
 import { NavLink } from "react-router-dom";
 import Header from '../header/header';
 import Noticeboard from './noticeborad/noticeboard';
+import Pick from './pick/pick';
 
 function Mainroot() {
-
-
   return (
-    <div>
+    <div className={styles.container}>
       <Header />
-      <Noticeboard />
-      <NavLink to='/allroot'>
-        <div className={styles.Look_Around_Box}>각 지역축제 더보기</div>
-      </NavLink>
+      <div className={styles.scroll}>
+        <div className={styles.pickTitle}>MoItzy-Pick</div>
+        <Pick className={styles.pick} />
+        <div className={styles.btn_location}>
+          <Noticeboard />
+          <NavLink to='/allroot'>
+            <div className={styles.Look_Around_Box}>각 지역축제 더보기</div>
+          </NavLink>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Mainroot
+export default Mainroot;
---
+++ src/components/main/pick/pick.jsx
@@ -0,0 +1,49 @@
+﻿import { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from './pick.module.css';
+
+function Pick() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [festivals, setFestivals] = useState([]);
+
+  useEffect(() => {
+    const fetchDataFromAPI = async () => {
+      try {
+        const response = await axios.post('http://localhost:8080/mo-itzy/festivals', { keyword: '', page: 0, size: 5 });
+        console.log("데이터 셋~", response.data.data.content); // 콘솔에 데이터 출력
+        setFestivals(response.data.data.content || []);
+      } catch (error) {
+        console.error("Error fetching festivals:", error);
+      }
+    };
+
+    fetchDataFromAPI();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex(prevIndex => (prevIndex + 1) % festivals.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [festivals]);
+
+  return (
+    <div className={styles.slideshow}>
+      <div className={styles.slide}>
+        {festivals.length === 0 ? (
+          <p>데이터가 없습니다.</p>
+        ) : (
+          <>
+            <img src={festivals[currentImageIndex].img} className={styles.photo} alt="Festival" />
+            <div className={styles.caption}>
+              <p>{festivals[currentImageIndex].name}</p>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Pick;
