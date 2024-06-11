98c3ac56fca6769cd32096c6a4de147f545bfa51
hyeon0820 <13whdduq13@naver.com>
2024-06-11 22:10:32 +0900

    correction

--- src/components/profile/mainprofile/mainprofile.jsx
+++ src/components/profile/mainprofile/mainprofile.jsx
@@ -6,11 +6,11 @@ function MainProfile() {
       <div>
           <div className={styles.main_box}>
               <div className={styles.profile}></div>
-              <div className={styles.name}>OOO님</div>
+              <div className={styles.name}>로그인해주세요</div>
               <div className={styles.review}>작성하신 리뷰 : 4개</div>
               <div className={styles.location}>
                   <div className={styles.change}>회원정보 수정</div>
-                  <div className={styles.logout}>로그아웃</div>
+                  <div className={styles.logout}>로그인</div>
               </div>
           </div>
         </div>
